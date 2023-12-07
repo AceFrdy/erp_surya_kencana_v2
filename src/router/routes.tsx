493bbaa30c70c6bd9437c8d9629179da7f15a80b
AceFrdy <92746931+AceFrdy@users.noreply.github.com>
2023-12-07 14:39:11 +0700

    kategori produk selesai

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -1,9 +1,11 @@
 import { lazy } from 'react';
 const Index = lazy(() => import('../pages/Index'));
 const Ecommerce = lazy(() => import('../pages/Ecommerce'));
-const Kategoriproduk = lazy(() => import('../pages/Basic'))
+const Kategoriproduk = lazy(() => import('../pages/KategoriProduk'))
 const Produk = lazy(() => import('../pages/MenuPenjualan/MultiColumn'))
 const Unit = lazy(() => import('../pages/MenuPenjualan/Unit'))
+// const AddProduk = lazy(() => import('../pages/MenuPenjualan/'))
+
 
 
 const routes = [
@@ -24,6 +26,11 @@ const routes = [
         element: <Produk />,
         layout: 'default',
     },
+    // {
+    //     path: '/menupenjualan/product/produk/add',
+    //     element: <AddProduk/>,
+    //     layout: 'default',
+    // },
     {
         path: '/menupenjualan/product/kategoriproduk',
         element: <Kategoriproduk />,
