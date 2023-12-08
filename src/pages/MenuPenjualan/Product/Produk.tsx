fc1ce6d58f5eeb7a1d66cf9a201ea76643df35e8
AceFrdy <92746931+AceFrdy@users.noreply.github.com>
2023-12-08 13:50:12 +0700

    hapus dialog

--- src/pages/MenuPenjualan/Product/Produk.tsx
+++ src/pages/MenuPenjualan/Product/Produk.tsx
@@ -629,49 +629,6 @@ const Produk = () => {
 
     return (
         <div>
-            <Transition appear show={hapusProduk} as={Fragment}>
-                <Dialog as="div" open={hapusProduk} onClose={() => setHapusProduk(false)}>
-                    <Transition.Child as={Fragment} enter="ease-out duration-300" enterFrom="opacity-0" enterTo="opacity-100" leave="ease-in duration-200" leaveFrom="opacity-100" leaveTo="opacity-0">
-                        <div className="fixed inset-0" />
-                    </Transition.Child>
-                    <div className="fixed inset-0 bg-[black]/60 z-[999] overflow-y-auto">
-                        <div className="flex items-start justify-center min-h-screen px-4">
-                            <Transition.Child
-                                as={Fragment}
-                                enter="ease-out duration-300"
-                                enterFrom="opacity-0 scale-95"
-                                enterTo="opacity-100 scale-100"
-                                leave="ease-in duration-200"
-                                leaveFrom="opacity-100 scale-100"
-                                leaveTo="opacity-0 scale-95"
-                            >
-                                <Dialog.Panel as="div" className="panel border-0 p-0 rounded-lg overflow-hidden my-8 w-full max-w-lg text-black dark:text-white-dark">
-                                    <div className="flex bg-[#fbfbfb] dark:bg-[#121c2c] items-center justify-between px-5 py-3">
-                                        <div className="text-lg font-bold">Hapus Produk</div>
-                                    </div>
-                                    <div className="p-5">
-                                        <div>
-                                            <form className="space-y-5">
-                                                <div>
-                                                    <h1>Apakah Anda yakin ingin menghapus Kategori</h1>
-                                                </div>
-                                            </form>
-                                        </div>
-                                        <div className="flex justify-end items-center mt-8">
-                                            <button type="button" className="btn btn-outline-danger" onClick={() => setHapusProduk(false)}>
-                                                Kembali
-                                            </button>
-                                            <button type="button" className="btn btn-primary ltr:ml-4 rtl:mr-4" onClick={() => setHapusProduk(false)}>
-                                                Hapus
-                                            </button>
-                                        </div>
-                                    </div>
-                                </Dialog.Panel>
-                            </Transition.Child>
-                        </div>
-                    </div>
-                </Dialog>
-            </Transition>
             <ul className="flex space-x-2 rtl:space-x-reverse">
                 <li>
                     <Link to="/" className="text-primary hover:underline">
