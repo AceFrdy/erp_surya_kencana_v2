205877bfd95040cc35ee3a7d46d7304f9c6b3200
shiqocred <21103080090@student.uin-suka.ac.id>
2023-12-15 16:18:15 +0700

    distribusi

--- src/providers/modal-provider.tsx
+++ src/providers/modal-provider.tsx
@@ -4,6 +4,8 @@ import DeleteUnitModal from '../components/modals/delete-modal-unit';
 import DeleteAkunModal from '../components/modals/delete-modal-akun';
 import DeleteProductModal from '../components/modals/delete-product-modal';
 import DeleteDataDistribusiModal from '../components/modals/delete-data-distribusi-modal';
+import DeleteSeluruhDistribusiModal from '../components/modals/delete-seluruh-distribusi-modal';
+import EditDistribusiModal from '../components/modals/edit-distribusi-modal';
 
 const ModalProvider = () => {
     return (
@@ -14,6 +16,8 @@ const ModalProvider = () => {
             <DeleteAkunModal />
             <DeleteProductModal />
             <DeleteDataDistribusiModal />
+            <DeleteSeluruhDistribusiModal />
+            <EditDistribusiModal />
         </>
     );
 };
