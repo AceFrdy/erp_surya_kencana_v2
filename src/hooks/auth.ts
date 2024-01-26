5dc3265ec6b8c78bf3046f669fe8de07296de06d
shiqocred <21103080090@student.uin-suka.ac.id>
2024-01-26 23:07:29 +0700

    fix middleware

--- src/hooks/auth.ts
+++ src/hooks/auth.ts
@@ -12,14 +12,19 @@ export const useAuth = () => {
     const [authorize, setAuthorize] = useState<string>('isLoading' || 'isAuthorized' || 'isUnauthorized');
     useEffect(() => {
         setAuthorize('isLoading');
-        axios
-            .get('https://erp.digitalindustryagency.com/api/user-profile', { headers: { Accept: 'application/json', Authorization: `Bearer ${token}` } })
-            .then((response) => {
-                setAuthorize('isAuthorized');
-            })
-            .catch((err) => {
-                setAuthorize('isUnauthorized');
-            });
+        const token = localStorage.getItem('accessToken');
+        if (token) {
+            axios
+                .get('https://erp.digitalindustryagency.com/api/user-profile', { headers: { Accept: 'application/json', Authorization: `Bearer ${token}` } })
+                .then((response) => {
+                    setAuthorize('isAuthorized');
+                })
+                .catch((err) => {
+                    setAuthorize('isUnauthorized');
+                });
+        } else {
+            setAuthorize('isUnauthorized');
+        }
     }, []);
     return authorize;
 };
