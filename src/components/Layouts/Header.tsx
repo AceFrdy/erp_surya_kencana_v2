f596fa76c88dc900153166abcf74629b81bbd2a5
AceFrdy <92746931+AceFrdy@users.noreply.github.com>
2024-02-12 11:42:39 +0700

    menghapus bagian yg kosong

--- src/components/Layouts/Header.tsx
+++ src/components/Layouts/Header.tsx
@@ -299,7 +299,7 @@ const Header = () => {
                                         </Link>
                                     </li>
                                     <li>
-                                        <Link to="/apps/mailbox" className="dark:hover:text-white">
+                                        {/* <Link to="/apps/mailbox" className="dark:hover:text-white">
                                             <svg className="ltr:mr-2 rtl:ml-2 shrink-0" width="18" height="18" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                                 <path
                                                     opacity="0.5"
@@ -315,10 +315,10 @@ const Header = () => {
                                                 />
                                             </svg>
                                             Inbox
-                                        </Link>
+                                        </Link> */}
                                     </li>
                                     <li>
-                                        <Link to="/auth/boxed-lockscreen" className="dark:hover:text-white">
+                                        {/* <Link to="/auth/boxed-lockscreen" className="dark:hover:text-white">
                                             <svg className="ltr:mr-2 rtl:ml-2 shrink-0" width="18" height="18" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                                 <path
                                                     d="M2 16C2 13.1716 2 11.7574 2.87868 10.8787C3.75736 10 5.17157 10 8 10H16C18.8284 10 20.2426 10 21.1213 10.8787C22 11.7574 22 13.1716 22 16C22 18.8284 22 20.2426 21.1213 21.1213C20.2426 22 18.8284 22 16 22H8C5.17157 22 3.75736 22 2.87868 21.1213C2 20.2426 2 18.8284 2 16Z"
@@ -339,7 +339,7 @@ const Header = () => {
                                                 </g>
                                             </svg>
                                             Lock Screen
-                                        </Link>
+                                        </Link> */}
                                     </li>
                                     <li className="border-t border-white-light dark:border-white-light/10">
                                         <button onClick={handleSignOut} className="text-danger !py-3">
